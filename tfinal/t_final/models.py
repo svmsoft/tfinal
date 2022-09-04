d5b8f29d2281130b5cef421efb66ccffb0e78a20
svmsoft <tinchosur@gmail.com>
2022-09-04 20:04:39 -0300

    Update models.py

--- tfinal/t_final/models.py
+++ tfinal/t_final/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class clientes(models.Model):
+    nombre = models.CharField(max_length=50)
+    dni = models.CharField()
+    fecha_nac = models.DateField()
+    domicilio = models.CharField()
+    telefono = models.CharField()
+    
+class rents(models.Model):
+    id_cliente = models.IntegerField()
+    id_material = models.IntegerField()
+    fecha_alqu = models.DateField()
+    hora_alq = models.DateField()
+    fecha_dev_est = models.DateField()
+    hora_dev_est = models.DateField()
+    recargo = models.BooleanField()
+    devuelto = models.BooleanField()
+    pagado = models.BooleanField()
+
+class material(models.Model):
+    codigo = models.CharField()
+    descripcion = models.CharField()
+    id_tipo = models.IntegerField()
+    
+class tipo(models.Model):
+    nombre = models.CharField()
+    
+    
+    
