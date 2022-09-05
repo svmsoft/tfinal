6da0a9f8b87c8ee82e877dda2cca121a23991744
svmsoft <tinchosur@gmail.com>
2022-09-05 15:53:48 -0300

    3

--- tfinal/t_final/views.py
+++ tfinal/t_final/views.py
@@ -1,3 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. VISTAS DE MIPRIMER MVT
+def principal(request):
+   nombre = miembros.objects.create(nombre = "Jorge",saldo = 1000000, fecha_nac = "1958-09-05")
+   context = {'miembro':miembros}
+
+
+   return render(request, 'principal.html', context={})
+
+def lista(request):
+     miembro= miembros.objects.all()
+     context = {'array': miembro}
+     return render(request, "lista.html", context={})
--- tfinal/tfinal/urls.py
+++ tfinal/tfinal/urls.py
@@ -15,7 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from t_final.views import principal
+from t_final.views import n_alquiler
+from t_final.views import a_lista
+from t_final.views import devoluciones
+from t_final.views import n_cliente
+from t_final.views import c_lista
+from t_final.views import n_material
+from t_final.views import m_lista
+from t_final.views import tipos
+from t_final.views import t_lista
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('principal/',principal),
+    path('n_alquiler/',n_alquiler),
+    path('a_lista/',a_lista),
+    path('devoluciones/',devoluciones),
+    path('n_cliente',n_cliente),
+    path('c_lista/',c_lista),
+    path('n_material/',n_material),
+    path('m_lista/',m_lista),
+    path('a_lista/',a_lista),
+    path('tipos/',tipos),
+    path('t_lista/',t_lista),
 ]
